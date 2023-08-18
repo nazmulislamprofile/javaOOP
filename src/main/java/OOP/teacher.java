8a23c18893aa14ed0691a30d51fe86d74642c6bb
pc <pc@Kulsum>
2023-08-18 08:02:33 +0600

    OOP learning started

---
+++ src/main/java/OOP/teacher.java
@@ -0,0 +1,21 @@
+
+package OOP;
+
+/**
+ *
+ * @author pc
+ */
+public class teacher {
+    String name;
+    int age;
+    int phoneNumber;
+    
+   void display(){
+       System.out.println("name ="+name);
+       System.out.println("age ="+age);
+       System.out.println("phone Number="+phoneNumber);
+       
+   }
+    
+    
+ }
---
+++ src/main/java/OOP/test.java
@@ -0,0 +1,31 @@
+
+package OOP;
+
+public class test {
+   
+  
+    
+    // main method
+    public static void main(String[] args){
+        teacher teacher1=new teacher();
+        
+        teacher1.name="nazmul islam";
+        teacher1.age=23;
+        teacher1.phoneNumber=1973961381;
+
+        /*
+        System.out.print( teacher1.name);
+        System.out.print( teacher1.age);
+        System.out.print( teacher1.phoneNumber);
+      
+        */
+        teacher1.display();
+        
+        
+        
+    }
+    
+    
+    
+    
+}
