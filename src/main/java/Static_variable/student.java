7da2677a937147abad23c99547fec49eab0a07da
pc <pc@Kulsum>
2023-08-23 20:50:25 +0600

    static variable

---
+++ src/main/java/Static_variable/student.java
@@ -0,0 +1,17 @@
+
+package Static_variable;
+
+public class student {
+    
+  static  int count =0;  //test here static and without static
+    
+    student (){
+        count++;
+}
+   void display(){
+       System.out.println("student number =" +count); 
+    }
+    
+    
+    
+}
---
+++ src/main/java/Static_variable/test.java
@@ -0,0 +1,16 @@
+
+package Static_variable;
+
+public class test {
+  
+    public static void main(String[] args){
+        student s1=new student();
+        student s2=new student();
+        student s3=new student();
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+    
+    
+}
