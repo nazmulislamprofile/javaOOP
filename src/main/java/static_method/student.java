0701e68c1e7edbbf1237663ff9bf09d1c4bf2e6f
pc <pc@Kulsum>
2023-08-23 21:55:42 +0600

    static method

---
+++ src/main/java/static_method/student.java
@@ -0,0 +1,16 @@
+
+package static_method;
+
+
+public class student {
+    
+    void display(){
+        System.out.print("Hello my name is nazmul islam");
+    }
+    
+    
+    static void display2(){
+        System.out.println("hello my name is abid hasan abrar");
+    }
+    
+}
---
+++ src/main/java/static_method/test.java
@@ -0,0 +1,14 @@
+
+package static_method;
+
+
+public class test {
+    
+    public static void main(String[] args){
+        student s1=new student();
+        s1.display();
+        
+        student.display2();
+    }
+    
+}
