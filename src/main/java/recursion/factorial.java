ae27ca1752fef1da5bd4ccaa48e280415332b4ae
pc <pc@Kulsum>
2023-08-25 19:37:25 +0600

    recursion

---
+++ src/main/java/recursion/factorial.java
@@ -0,0 +1,16 @@
+
+package recursion;
+
+
+public class factorial {
+    
+    int fact(int n){
+        if(n==1){
+            return 1;
+        }
+        else{
+            return n*fact(n-1);
+        }
+    }
+    
+}
---
+++ src/main/java/recursion/test.java
@@ -0,0 +1,14 @@
+
+package recursion;
+
+
+public class test {
+    
+    public static void main(String[] args){
+        
+        factorial obj =new factorial();
+        int result= obj.fact(5);
+        System.out.println("factorial="+result);
+    }
+      
+}
