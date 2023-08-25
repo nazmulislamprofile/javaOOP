3bf1655ca2ed02b034303394c52e5d9a2301dfff
pc <pc@Kulsum>
2023-08-25 07:34:48 +0600

    method overloading

---
+++ src/main/java/methodOverloading/overloadTest.java
@@ -0,0 +1,19 @@
+
+package methodOverloading;
+
+
+public class overloadTest {
+   
+    public static void main(String[] args){
+        methodOverload obj =new methodOverload();
+        
+        obj.add();
+        obj.add(2,3);
+        obj.add(2.5,3.5);
+        obj.add(2,3,5);
+        
+    }
+    
+    
+    
+}
