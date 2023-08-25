03ed0a4a4d3729f04ca862db39bd85840101f92b
pc <pc@Kulsum>
2023-08-25 09:43:06 +0600

    debug test

---
+++ src/main/java/debug/test.java
@@ -0,0 +1,17 @@
+
+package debug;
+
+public class test {
+    public static void main(String[] args){
+    int sum=0;
+    for(int i=0; i<10; i++){
+    sum=sum+i;
+    
+    }
+    
+    System.out.println("sum ="+sum);
+    
+  }
+    
+    
+}
