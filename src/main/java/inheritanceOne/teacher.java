83f61213e594931b07fff4161a58ce804dd5d14d
pc <pc@Kulsum>
2023-08-26 18:11:07 +0600

    inheritance another example

---
+++ src/main/java/inheritanceOne/teacher.java
@@ -0,0 +1,24 @@
+
+package inheritanceOne;
+
+
+public class teacher extends person {
+    private String qualification;
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+    
+    void display(){
+         System.out.println(getName());
+        System.out.println(getAge());
+        System.out.println(getQualification());
+        System.out.println("\n \n");
+    }
+    
+    
+}
---
+++ src/main/java/inheritanceOne/test.java
@@ -0,0 +1,24 @@
+
+package inheritanceOne;
+
+public class test {
+    
+    public static void main(String[] args){
+        teacher t1=new teacher();
+        teacher t2=new teacher();
+        
+        t1.setName("nazmul islam");
+        t1.setAge(23);
+        t1.setQualification("Bse in CSE");
+        t1.display();
+        
+        t2.setName("abid hasan");
+        t2.setAge(2);
+        t2.setQualification("Bse in CSE");
+        t2.display();
+ 
+        
+    }
+    
+    
+}
