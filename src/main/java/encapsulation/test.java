c26477a8c2520554a0351aa41ef05e5b294786bf
pc <pc@Kulsum>
2023-08-26 16:49:34 +0600

    encapsulation

---
+++ src/main/java/encapsulation/test.java
@@ -0,0 +1,14 @@
+
+package encapsulation;
+
+public class test {
+    public static void main(String[] args){
+        person p1 =new person();
+        p1.setName("nazmul islam");
+        System.out.println(p1.getName());
+        p1.setAge(23);
+        System.out.println(p1.getAge());
+    }
+    
+    
+}
