049a610cbf988a7a1110e9f9f5ec9c79bd6eb89e
nazmulislamprofile <74948943+nazmulislamprofile@users.noreply.github.com>
2024-01-02 09:32:43 +0000

    prime number added with method

--- src/main/java/Progrm_primeNumber/PrimeNumberOne.java
+++ src/main/java/Progrm_primeNumber/PrimeNumberOne.java
@@ -1,29 +1,23 @@
 package Progrm_primeNumber;
 
 public class PrimeNumberOne {
-    public static void main(String[] args) {
-        int n, m;
-        n = 12;
+    public static void main(String args[]) {
+        int i, m = 0, flag = 0;
+        int n = 3;// it is the number to be checked
         m = n / 2;
-        boolean flag;
-        flag = false;
-
         if (n == 0 || n == 1) {
-            System.out.println(n + "is prime number");
-        }
-         else {
-            for (int i = 2; i < m; i++) {
+            System.out.println(n + " is not prime number");
+        } else {
+            for (i = 2; i <= m; i++) {
                 if (n % i == 0) {
-                    System.out.println(n + "is not prime number");
-                    flag = true;
+                    System.out.println(n + " is not prime number");
+                    flag = 1;
                     break;
                 }
-                if (!flag) {
-                    System.out.println(n + "is prime number");
-                }
-
             }
-        }
-
+            if (flag == 0) {
+                System.out.println(n + " is prime number");
+            }
+        } // end of else
     }
 }
